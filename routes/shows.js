9a8ea2ab3d82a58af60b62226739dbe337285f2d
vishnukumar <vishnu.n@skil.ai>
2024-10-27 09:48:13 +0530

    router level middlewares added for authentication

--- routes/shows.js
+++ routes/shows.js
@@ -1,6 +1,43 @@
 const express = require("express")
 const router = express.Router();
 const utils = require("../utils")
+const jwt = require("jsonwebtoken")
+
+router.use(function(req, res, next){
+  let headers = req.headers;
+  let userId = ""
+  if(headers && headers.authorization){
+      let tokenParts = headers.authorization.split(" ");
+      if(Array.isArray(tokenParts) && tokenParts.length>=2){
+          let accessToken = tokenParts[1];
+          jwt.verify(accessToken,process.env.SECRET_KEY,function(err, result){
+              if(err){
+                  return res.json({
+                      status:"error",
+                      message:"Error occurred while processing the request",
+                      error:err
+                  })
+              }
+              else{
+                  req.body["userId"] = result["userId"] || "";
+                  next()
+              }
+          })
+      }
+      else{
+          return res.json({
+              status:"error",
+              message:"Error occurred while processing the request",
+          })            
+      }
+  }
+  else{
+      return res.json({
+          status:"error",
+          message:"Error occurred while processing the request",
+      })        
+  }
+})
 
 router.post("/fetch-show-theatres",function(req, res){
   utils.shows.fetchShowTheatres(req, function(err, result){
